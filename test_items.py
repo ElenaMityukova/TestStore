a772c32b433b54f5f5bf8909ba314fd22cad929a
ElenaMityukova <124844232+ElenaMityukova@users.noreply.github.com>
2023-05-23 23:05:45 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,6 +6,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_button_add_to_basket(browser):
     browser.get(link)
+    #sleep(30)
     add_to_basket = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     sleep(3)
     assert add_to_basket
